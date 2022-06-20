864d491d118389ea707b8b46d1d2d30f8d6980ea
danielaboso <daniela_boso@hotmail.com>
2022-06-19 22:30:34 -0300

    desafio complementario

---
+++ indice.js
@@ -0,0 +1,8 @@
+
+let numero = parseInt(prompt("Ingrese el numero de la tabla que quiere ver"))
+for(let i=1; i<=10; i++){
+    let resultado = numero*i
+    console.log(`${i}x ${numero} = ${resultado}`)
+}
+
+
